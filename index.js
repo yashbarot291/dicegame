c0b199101fd0d750f6e20bdd0ca78b2f585bbafd
Yash Barot <96978920+yashbarot291@users.noreply.github.com>
2022-02-18 18:44:41 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+var r1= Math.floor(Math.random()*6)+1;
+var nwimg1= "images/dice"+r1+".png";
+var r2=  Math.floor(Math.random()*6)+1;
+var nwimg2= "images/dice"+r2+".png";
+document.querySelector(".img1").setAttribute("src", nwimg1);
+document.querySelector(".img2").setAttribute("src", nwimg2);
+if(r1>r2){
+  document.querySelector("h1").textContent="🚩Player 1 wins";
+}
+else if(r1<r2){
+  document.querySelector("h1").textContent="Player 2 wins🚩";
+}
+else{
+  document.querySelector("h1").textContent="Draw!";
+}
